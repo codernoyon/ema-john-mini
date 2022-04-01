a66d33157b40bc1284f493906d657deeed1f2ff4
codernoyon <noyonrahman2003@gmail.com>
2022-04-01 18:17:57 +0600

    some update on responsive layout

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -7,7 +7,7 @@ const Order = () => {
     return (
         <section className="order-section py-3">
             <div className="container">
-                <div className="row justify-content-between align-items-center px-3 ">
+                <div className="row justify-content-between align-items-center px-2 ">
                     <div className="col-lg-7 order-products ps-1 pe-2 mb-3">
                         <OrderProduct></OrderProduct>
                         <OrderProduct></OrderProduct>
--- src/components/OrderProduct/OrderProduct.js
+++ src/components/OrderProduct/OrderProduct.js
@@ -7,20 +7,19 @@ const OrderProduct = () => {
     return (
         <div className='d-flex order-product'>
             <div className='order-product-img p-1'>
-                <img className='w-100' src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/96a5f085fedf4e678095abad01056711_9366/Lite_Racer_Adapt_3.0_Shoes_Black_FX8802_01_standard.jpg" alt="" />
+                <img className='w-100' src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/96a5f085fedf4e678095abad01056711_9366/Lite_Racer_Adapt_3.0_Shoes_Black_FX8802_01_standard.jpg" alt=""/>
             </div>
-            <div className="order-product-details align-items-center d-flex ps-2 pe-3 justify-content-between w-100">
+            <div className="order-product-details align-items-center d-flex ps-2 pe-3 justify-content-between w-100 ">
                 <div className="info">
-                    <h5>LITE RACER ADAPT 3...</h5>
+                    <h6>LITE RACER ADAPT 3...</h6>
                     <p className='mb-0'>Price: $<span className='highlight'>200</span></p>
                     <p className='mb-0'>Quantity <span className='highlight'>1</span></p>
-                    <p className='mb-0'>Shipping Charge: $<span className='highlight'>5</span></p>
+                    <small className='mb-0'>Shipping Charge: $<span className='highlight'>5</span></small>
                 </div>
                 <button className='delete-btn'>
                     <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
                 </button>
             </div>
-            
         </div>
     );
 };
