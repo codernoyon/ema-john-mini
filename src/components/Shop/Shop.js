1d8097383c29f110f85cb6befb70a70b0c9687a5
codernoyon <noyonrahman2003@gmail.com>
2022-04-28 16:58:16 +0600

    paginatio added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,25 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import useCart from '../../hooks/useCart';
-import useProducts from '../../hooks/useProducts';
 import { addToDb, deleteShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import PageTitle from '../PageTitle/PageTitle';
 import Product from '../Product/Product';
 import "./Shop.css";
+import Loading from '../Loading/Loading'
 
 const Shop = () => {
-    const [products,] = useProducts();
+    const [products, setProducts] = useState([]);
     const [cart, setCart] = useCart(products);
     const [pagesCount, setPagesCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+    const [loading, setLoading] = useState(false);
+    
     useEffect(() => {
-        fetch('http://localhost:5000/productsCount')
-        .then(res => res.json())
-        .then(data => {
-            const count = data.count;
-            const pages = Math.ceil(count/10);
-            setPagesCount(pages);
-        })
-    }, [])
+        fetch(`http://localhost:5000/products?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+        setLoading(true);
+    }, [page, size]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/productCount')
+            .then(res => res.json())
+            .then(data => {
+                const count = data.count;
+                const pages = Math.ceil(count / 10);
+                setPagesCount(pages);
+            })
+    }, []);
+
+    console.log(page);
 
 
     const handleAddToCart = (selectedProduct) => {
@@ -46,6 +60,10 @@ const Shop = () => {
         deleteShoppingCart()
     }
 
+    if(loading){
+        return <Loading></Loading>
+    }
+
     return (
         <div className="shop">
             <PageTitle title={"Shop"}></PageTitle>
@@ -63,11 +81,24 @@ const Shop = () => {
                                 ></Product>)
                             }
                         </div>
-                        <div className='py-3 d-flex w-100 align-items-center justify-content-center'>
-                        {
-                            [...Array(pagesCount).keys()]
-                            .map(number => <button className='mx-1 py-1 px-4 btn btn-warning'>{number + 1}</button>)
-                        } 
+                        <div className="d-flex bg-danger align-items-center justify-content-center px-4 ">
+                            <div className='py-3 d-flex w-100 align-items-center justify-content-center'>
+                                {
+                                    [...Array(pagesCount).keys()]
+                                        .map(number => <button
+                                            onClick={() => setPage(number)}
+                                            key={number}
+                                            className={page === number ? 'selected' : 'paginate-btn'}>{number}</button>)
+                                }
+                            </div>
+                            <div className='w-25'>
+                                <select onChange={(e) => setSize(e.target.value)} className='w-100'>
+                                    <option value="5">5</option>
+                                    <option selected value="10">10</option>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                     <div className="col-lg-3 pt-2 cart-container order-first order-lg-last mt-5 rounded-1">
